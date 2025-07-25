42eebe4c02513e458b9f937e8969c3c6691ef714
Topaka <tommyaa@gmail.com>
2025-07-25 23:17:07 +0200

    Updated type effectivenesses

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -299,6 +299,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
         .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
+        .category = CATEGORY_SPECIAL
     },
 
     [MOVE_STOMP] =
